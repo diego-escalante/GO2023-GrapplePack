a592bfa943508747f4c014723b4bb1ee463433dd
Diego Escalante <diego.escalante01@gmail.com>
2023-11-20 22:39:17 -0700

    Rework how grapple extends, hooks, and retracts

--- objects/grapple/grapple.gd
+++ objects/grapple/grapple.gd
@@ -1,107 +1,139 @@
 extends Node2D
 class_name Grapple
 
-signal extended(is_attached)
-signal pulled
+enum State {IDLE, EXTENDING, HOOKED, RETRACTING}
 
-enum State {IDLE, EXTENDING, EXTENDED, RETRACTING, PULLING}
+@export var input_enabled := true
+@export var speed := 50.0
+@export var grapple_length := 10.0
 
-@export var _grapple_speed := 60.0
-@export var _input_enabled = true
-@export var _grapple_length := 5.0
+var _pointQueryParams := PhysicsPointQueryParameters2D.new()
+var _state := State.IDLE :
+	set(val):
+		visible = val != State.IDLE
+		_state = val
+var _target := Vector2.ZERO
+var _is_target_grappable := false
 
-var target: Vector2
-var _state := State.IDLE
-var _grapple_total_time: float
-var _grapple_time: float
-var _can_attach: bool
+@onready var hook := $Hook as CharacterBody2D
+@onready var hook_raycast := $Hook/RayCast2D as RayCast2D
+@onready var chain := $Chain as Line2D
 
-@onready var _chain := $Chain as Line2D
-@onready var _hook := $Hook as Sprite2D
-@onready var _raycast := $RayCast2D as RayCast2D
 
-func _unhandled_input(event):
-	if event.is_action_pressed("grapple") and _state == State.IDLE:
-		extend(get_global_mouse_position())
-		pass
-	elif event.is_action_released("grapple") and (_state == State.EXTENDING or _state == State.EXTENDED):
-		print("retract")
-		# Retract
-		pass 
+func _ready() -> void:
+	_pointQueryParams.collide_with_areas = true
+	_pointQueryParams.collide_with_bodies = false
+	_pointQueryParams.collision_mask = 4 # Grappleable areas are on Layer 3, Bit 2 (zero-indexed).
+	hook.top_level = true
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("grapple") and _state == State.IDLE and input_enabled:
+		_set_target()
+		_state = State.EXTENDING
+		hook.global_position = global_position
 		
-	
+	elif event.is_action_released("grapple") and _state != State.IDLE:
+		_state = State.IDLE
 
-func extend(to: Vector2) -> void:
-	_cast_ray(to)
-	_state = State.EXTENDING
-	_grapple_total_time = global_position.distance_to(target) / GameConsts.PIXELS_PER_UNIT / _grapple_speed
-	_grapple_time = 0
-	visible = true
-	
-
-func retract() -> void:
-	if _state != State.IDLE or _state != State.RETRACTING:
-		_state = State.RETRACTING
-		_grapple_total_time = global_position.distance_to(target) / GameConsts.PIXELS_PER_UNIT / _grapple_speed
-		_grapple_time = _grapple_total_time 
-
-func pull() -> void:
-	if _state != State.IDLE:
-		_state = State.PULLING
-func is_ready() -> bool:
-	return _state == State.IDLE
 
 func _physics_process(delta: float) -> void:
-	match _state:
+	match _state: 
 		State.IDLE:
-			pass
-
-		State.PULLING:
-			_set_grapple_head(target)
-			if target == global_position:
-				_state = State.IDLE
-				visible = false
-				pulled.emit()
-
+			return
 		State.EXTENDING:
-			_grapple_time = min(_grapple_time + delta, _grapple_total_time)
-			var current_position := global_position.lerp(target, _grapple_time/_grapple_total_time)
-			_set_grapple_head(current_position)
-			if _grapple_time == _grapple_total_time:
-				_state = State.EXTENDED
-				extended.emit(_can_attach)
-
-		State.EXTENDED:
-			_set_grapple_head(target)
-
+			_extend_process(delta)
+		State.HOOKED:
+			_hooked_process(delta)
 		State.RETRACTING:
-			_grapple_time = max(0, _grapple_time - delta)
-			if _grapple_time == 0:
-				_state = State.IDLE
-				visible = false
-			_set_grapple_head(global_position.lerp(target, _grapple_time/_grapple_total_time))
+			_retract_process(delta)
 
-func _cast_ray(to: Vector2):
-	_raycast.enabled = true
-	_raycast.target_position = to_local(position.direction_to(to) * _grapple_length)
-	_raycast.force_raycast_update()
+
+func _extend_process(delta: float) -> void:	
 	
-	if not _raycast.is_colliding():
-		# If the grapple won't hit anything, extend it the full way, but don't attach.
-		_can_attach = false
-		target = to
-	elif _raycast.get_collider() is GrappleArea:
-		# If the grapple hits a grapple area, extend it to a grapple area's grapple point, attach.
-		_can_attach = true
-		target = (_raycast.get_collider() as GrappleArea).get_grapple_point(to)
+	var motion := hook.global_position.direction_to(_target) * speed * delta * GameConsts.PIXELS_PER_UNIT
+	var length_traveled := hook.global_position.distance_to(global_position)
+	
+	# Make sure to cap the length of the grapple.
+	if length_traveled + motion.length() > grapple_length * GameConsts.PIXELS_PER_UNIT:
+		# if the hook does not catch anything this update, it will be retracting on the next one.
+		_state = State.RETRACTING
+		# cap the motion this update to not overshoot.
+		motion = hook.global_position.direction_to(_target) * (grapple_length * GameConsts.PIXELS_PER_UNIT - length_traveled)
+	
+	# Rotate the hook accordingly. This must happen before raycasting, as the raycast direction is
+	# forward relative to the hook's rotation.
+	hook.look_at(_target)
+	
+	# Raycast to the new position.
+	hook_raycast.target_position = Vector2.RIGHT * motion.length()
+	hook_raycast.force_raycast_update()
+	if hook_raycast.is_colliding():
+		if hook_raycast.get_collider() is GrappleArea:
+			# Hit a grappleable.
+			hook.global_position = (hook_raycast.get_collider() as GrappleArea).get_grapple_point(hook_raycast.get_collision_point())
+			_state = State.HOOKED
+		
+		else:
+			# Hit a wall.
+			hook.global_position = hook_raycast.get_collision_point()
+			_state = State.RETRACTING
 	else:
-		# if the grapple hits a wall, extend to the wall, but don't attach.
-		_can_attach = false
-		target = _raycast.get_collision_point()
-	_raycast.target_position = Vector2.ZERO
-	_raycast.enabled = false
+		# Nothing impeding the hook.
+		if motion.length() > hook.global_position.distance_to(_target):
+			hook.global_position = _target
+			_state = State.RETRACTING
+		else:
+			hook.global_position += motion
 	
+	# Update chain.
+	chain.set_point_position(0, to_local(hook.global_position))
 
-func _set_grapple_head(pos: Vector2) -> void:
-	pass
-#	set_point_position(1, to_local(pos))
+func _hooked_process(delta: float) -> void:
+	# Rotate hook accordingly.
+	hook.look_at(global_position)
+	hook.rotation_degrees += 180
+	
+	# Update chain.
+	chain.set_point_position(0, to_local(hook.global_position))
+
+
+func _retract_process(delta: float) -> void:
+	# Move the hook's position.
+	var motion := hook.global_position.direction_to(global_position) * speed * delta * GameConsts.PIXELS_PER_UNIT
+	if motion.length() > hook.global_position.distance_to(global_position):
+		hook.global_position = global_position
+		_state = State.IDLE
+	else:
+		hook.global_position += motion
+	
+	# Rotate the hook accordingly.
+	hook.look_at(global_position)
+	hook.rotation_degrees += 180
+	
+	# Update chain.
+	chain.set_point_position(0, to_local(hook.global_position))
+
+
+
+func _set_target() -> void:
+	var mouse_pos := get_global_mouse_position()
+	var grapple_area := _get_grapple_area(mouse_pos)
+	if grapple_area == null:
+		_target = mouse_pos
+		_is_target_grappable = false
+	else:
+		_target = grapple_area.get_grapple_point(mouse_pos)
+		_is_target_grappable = true
+
+
+func _get_grapple_area(at_position: Vector2) -> GrappleArea:
+	_pointQueryParams.position = at_position
+	var result := get_world_2d().direct_space_state.intersect_point(_pointQueryParams, 1)
+	if result.is_empty():
+		return null
+	return result.front().get("collider") as GrappleArea
+
+
+func get_direction() -> Vector2:
+	return global_position.direction_to(hook.global_position)
