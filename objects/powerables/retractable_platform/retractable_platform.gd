f80f71cb5abd774e4e887114add9b4937490c6e9
Diego Escalante <diego.escalante01@gmail.com>
2023-11-22 15:30:06 -0700

    Fix bug where retractable platforms become fully solid

--- objects/powerables/retractable_platform/retractable_platform.gd
+++ objects/powerables/retractable_platform/retractable_platform.gd
@@ -6,9 +6,9 @@ extends Powerable
 func _ready() -> void:
 	super()
 	_sprite.frame = 2 if _is_powered else 0
-	_static_body.set_collision_layer_value(1, _is_powered)
+	_static_body.set_collision_layer_value(4, _is_powered)
 
 	
 func _change() -> void:
 	_sprite.play("default", 1 if _is_powered else -1)
-	_static_body.set_collision_layer_value(1, _is_powered)
+	_static_body.set_collision_layer_value(4, _is_powered)
