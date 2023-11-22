0c224f57259c478809c93d9e24434a54e451cda0
Diego Escalante <diego.escalante01@gmail.com>
2023-11-22 01:12:51 -0700

    Finish Switches, implement Powerables

---
+++ objects/powerables/powerable.gd
@@ -0,0 +1,30 @@
+extends Node
+class_name Powerable
+
+signal changed(is_powered: bool)
+
+@export var _is_powered := false
+@export var _upstream: Powerable
+@export var _negate_upstream := false
+@export var _signal_delay := 0.25
+
+
+func _ready() -> void:
+	# Do not forget to use super() when defining a _ready() function in a powerable.
+	# Otherwise, the upstream powerable's signal will not be hooked up.
+	if _upstream != null:
+		_upstream.changed.connect(_on_changed)
+
+func _on_changed(is_powered: bool) -> void:
+	_is_powered = !is_powered if _negate_upstream else is_powered
+	if _signal_delay > 0:
+		get_tree().create_timer(_signal_delay).timeout.connect(func(): changed.emit(_is_powered))
+	else:
+		changed.emit(_is_powered)
+	_change()
+	
+
+func _change() -> void:
+	# To be overridden by Powerables.
+	pass
+
--- objects/switch/switch.gd
+++ objects/switch/switch.gd
@@ -1,8 +1,19 @@
-extends AnimatedSprite2D
+extends Powerable
 class_name Switch
 
+@onready var _sprite := $Sprite as AnimatedSprite2D
+
 func _ready() -> void:
+	# Switches do not use upstream power signals. No need to use super() here.
+	_sprite.animation = "to_on" if _is_powered else "to_off"
+	_sprite.frame = 1
 	($GrappleArea as Area2D).body_entered.connect(_on_grappled)
 
+
 func _on_grappled(_body: Node2D) -> void:
-	print("Triggered!")
+	# Switches are special, they do not change via upstream signals, but by the grapple.
+	_on_changed(!_is_powered)
+	
+
+func _change() -> void:
+	_sprite.play("to_on" if _is_powered else "to_off")
