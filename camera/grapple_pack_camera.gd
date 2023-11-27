a38c4bc16713bbd4821ce36850a77ac0e3ebea2a
Diego Escalante <diego.escalante01@gmail.com>
2023-11-26 18:42:42 -0700

    Complete the fall intro

---
+++ camera/grapple_pack_camera.gd
@@ -0,0 +1,11 @@
+extends ShakingCamera2D
+class_name GrapplePackCamera
+
+func _process(delta: float) -> void:
+	super(delta)
+	if global_position.x > limit_right + 9:
+		limit_right += 180 + 12
+		limit_left += 180 + 12
+	elif global_position.x < limit_left - 9:
+		limit_right -= 180 + 12
+		limit_left -= 180 + 12
